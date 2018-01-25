8d4ae530daaf7fbc8cbfaf3153792f69167d9866
samwisee <mboos@outlook.com>
2018-01-25 17:19:26 +0100

    downloader improved

--- core/downloader.py
+++ core/downloader.py
@@ -1,15 +1,16 @@
+import sys
+import os
 import overpy
-from PIL import Image, ImageDraw
 from pygeotile.tile import Tile
 from pygeotile.point import Point
 import requests
 import secrets
 import shapely.geometry as geometry
-import shapely.affinity as affinity
 import numpy as np
 from core.settings import IMAGE_WIDTH
 from skimage import draw
 import scipy.misc
+import shutil
 
 api = overpy.Overpass()
 
@@ -44,7 +45,10 @@ def tiles_from_bbox(bbox, zoom_level):
     return tiles
 
 
-def osm_downloader(bbox, zoom_level):
+def osm_downloader(bbox, zoom_level, output_directory):
+    if not os.path.isdir(output_directory):
+        os.makedirs(output_directory)
+
     tiles = tiles_from_bbox(bbox=bbox, zoom_level=zoom_level)
     response = requests.get("https://dev.virtualearth.net/REST/V1/Imagery/Metadata/Aerial?key={key}"
                             .format(key=secrets.BING_KEY))
@@ -53,65 +57,148 @@ def osm_downloader(bbox, zoom_level):
     tile_url_template = data['resourceSets'][0]['resources'][0]['imageUrl']
     subdomain = data['resourceSets'][0]['resources'][0]['imageUrlSubdomains'][0]
 
+    tiles_path = os.path.join(output_directory, 'tiles.txt')
+    loaded_tiles = []
+    if os.path.isfile(tiles_path):
+        with open(tiles_path, 'r', encoding="utf-8") as f:
+            lines = f.readlines()
+            loaded_tiles = list(map(lambda l: l[:-1], lines))  # remove '\n'
+    print(loaded_tiles)
+
     for i, t in enumerate(tiles):
-        if i < 2:
+        tms_x, tms_y = t.tms
+        tile_name = "{z}_{x}_{y}".format(z=zoom_level, x=tms_x, y=tms_y)
+        if tile_name in loaded_tiles:
             continue
 
         minx, maxy = t.bounds[0].pixels(zoom_level)
         maxx, miny = t.bounds[1].pixels(zoom_level)
         b = []
-        # tile_start = t.bounds[0].meters
-        # print(t.bounds[0].pixels(17))
-        # break
         b.extend(t.bounds[0].latitude_longitude)
         b.extend(t.bounds[1].latitude_longitude)
         url = tile_url_template.format(subdomain=subdomain, quadkey=t.quad_tree)
         query = query_template.format(bbox="{},{},{},{}".format(*b), tile=t.tms)
         print(url)
-        # print(query)
-
+        print(query)
         res = api.query(query)
-        polygons = []
         mask = np.zeros((IMAGE_WIDTH, IMAGE_WIDTH), dtype=np.uint8)
         for way in res.ways:
             points = []
             for node in way.nodes:
                 p = Point(float(node.lat), float(node.lon))
                 px = p.pixels(zoom=zoom_level)
-                # points.append((px[0]-tile_pixels[0], px[1]-t.bounds[1].pixels(zoom_level)[1]))
                 points.append((px[0]-minx, px[1]-miny))
             poly = geometry.Polygon(points)
             tile_rect = geometry.box(0, 0, IMAGE_WIDTH, IMAGE_WIDTH)
-            poly = poly.intersection(tile_rect)
-            print(poly.svg())
+            try:
+                poly = poly.intersection(tile_rect)
+            except:
+                print("Intersection failed for polygon and rectangle: poly='{}', box='{}'".format(poly, tile_rect))
+                continue
+            polygons = []
+            if isinstance(poly, geometry.MultiPolygon):
+                for p in poly.geoms:
+                    if isinstance(p, geometry.Polygon):
+                        polygons.append(p)
+                    elif isinstance(p, geometry.MultiPolygon):
+                        print(poly)
+            else:
+                polygons.append(poly)
+            update_mask(mask, polygons)
 
-            xs, ys = poly.exterior.coords.xy
-            rr, cc = draw.polygon(xs, ys, (IMAGE_WIDTH, IMAGE_WIDTH))
-            mask[cc, rr] = 255
-            polygons.append(poly)
-        scipy.misc.imsave("test.tiff", mask)
-        print("saved")
-        return
+        if res.ways and mask.max():
+            file_name = "{}.tif".format(tile_name)
+            mask_path = os.path.join(output_directory, file_name+'f')
+            img_path = os.path.join(output_directory, file_name)
+            scipy.misc.imsave(mask_path, mask)
+            if not os.path.isfile(img_path):
+                response = requests.get(url, stream=True)
+                response.raw.decode_content = True
+                with open(img_path, 'wb') as file:
+                    shutil.copyfileobj(response.raw, file)
+                del response
+        with open(os.path.join(output_directory, "tiles.txt"), 'a') as f:
+            f.write("{}\n".format(tile_name))
 
-        mask = np.zeros([IMAGE_WIDTH, IMAGE_WIDTH, 3], dtype=np.uint8)
-        # for poly in polygons:
-        #     polynp = np.array(poly)
-        #     print(polynp)
-            # print(poly.exterior.coords.xy)
 
-        # if res.ways:
-        #     break
-        # print(t.quad_tree)
-    # print(tiles)
+def update_mask(mask, polygons):
+    """
+     * The first polygon is the exterior ring. All others are treated as interior rings and will just invert
+       the corresponding area of the mask.
+    :param mask:
+    :param polygons:
+    :return:
+    """
+    for p in polygons:
+        if isinstance(p, geometry.MultiPolygon):
+            update_mask(mask, p.geoms)
+        elif not isinstance(p, geometry.Polygon):
+            continue
+        xs, ys = p.exterior.coords.xy
+        rr, cc = draw.polygon(xs, ys, (IMAGE_WIDTH, IMAGE_WIDTH))
+        mask[cc, rr] = np.invert(mask[cc, rr])
 
 
 if __name__ == "__main__":
-    bbox = {
-        'tl': 47.355106,
-        'tr': 8.518195,
-        'bl': 47.383125,
-        'br': 8.560596
+
+    bboxes = {
+        'zurich': {
+            'tl': 47.355106,
+            'tr': 8.518195,
+            'bl': 47.383125,
+            'br': 8.560596
+        },
+        'boston_financial_district': {
+            'tr':-71.07081,
+            'tl': 42.351557,
+            'br': -71.053601,
+            'bl': 42.362942
+        },
+        'boston_houses': {
+            'tr': -71.079802,
+            'tl': 42.280151,
+            'br': -71.05062,
+            'bl': 42.29958
+        },
+        'bern': {
+            'tr': 7.420455,
+            'tl': 46.935277,
+            'br': 7.46337,
+            'bl': 46.965862
+        },
+        'new_york': {
+            'tr': -74.02059,
+            'tl': 40.646089,
+            'br': -73.864722,
+            'bl': 40.77413
+        },
+        'munich': {
+            'tr': 11.465968,
+            'tl': 48.096287,
+            'br': 11.626643,
+            'bl': 48.189756
+        },
+        'rome': {
+            'tr': 12.449309,
+            'tl': 41.869589,
+            'br': 12.531363,
+            'bl': 41.923766
+        },
     }
-    zoom_levels = [18]
-    for z in zoom_levels:
-        osm_downloader(bbox=bbox, zoom_level=z)
+
+    print(sys.argv)
+    if len(sys.argv) > 1:
+        city = sys.argv[1]
+        valid_cities = bboxes.keys()
+        if city not in valid_cities:
+            raise RuntimeError("'{}' is not a valid city. Valid cities are: {}".format(city, valid_cities))
+        cities = [city]
+    else:
+        cities = bboxes.keys()
+
+    for bbox_name in cities:
+        print("Processing bbox '{}'".format(bbox_name))
+        bbox = bboxes[bbox_name]
+        zoom_levels = [18, 19]
+        for z in zoom_levels:
+            osm_downloader(bbox=bbox, zoom_level=z, output_directory=os.path.join("output", bbox_name))
