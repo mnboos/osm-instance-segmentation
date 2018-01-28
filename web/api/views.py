2b07f74d58d6e83fc36277ac156392d852becc24
samwisee <mboos@outlook.com>
2018-01-28 13:07:10 +0100

    django returns json

--- web/api/views.py
+++ web/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 
 @api_view(['GET'])
-def api_root(request, format=None):
-    return Response({'hello': 'world'})
+def api_root(request):
+    print("req: ", request)
+    return JsonResponse({'hello': 'world'})
