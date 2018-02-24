31f3515438b0529fff1317525f63cf9c9713377a
samwisee <mboos@outlook.com>
2018-02-24 12:15:23 +0100

    * refactoring * rest api returns rectangularized outlines

--- Dockerfile
+++ Dockerfile
@@ -7,16 +7,5 @@ RUN pip3 install -r requirements.txt
 RUN apt-get update -y &&\
     apt-get install -y git wget software-properties-common vim nano
 
-#RUN curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash &&\
-#    apt-get install -y git-lfs
-#
-#RUN git lfs install &&\
-#    git lfs track "*.h5" &&\
-#    git add .gitattributes
-#    add-apt-repository ppa:jonathonf/python-3.6 &&\
-#    apt-get update -y &&\
-#    apt-get install -y python3.6 python3-pip
-
-#ENTRYPOINT chmod +x ./execute.sh && ./execute.sh
+#ENTRYPOINT chmod +x ./execute && ./execute
 CMD exec /bin/bash -c "trap : TERM INT; sleep infinity & wait"
-#ENTRYPOINT ["/bin/bash"]
--- core/utils.py
+++ core/utils.py
@@ -5,7 +5,7 @@ from skimage.measure import approximate_polygon
 from skimage.transform import hough_line, hough_line_peaks
 from pygeotile.tile import Tile, Point
 from shapely.geometry import LineString
-from shapely.affinity import rotate, translate
+from shapely.affinity import rotate, scale
 from shapely import geometry
 import cv2
 import math
@@ -80,14 +80,17 @@ class Line:
         return self.__str__()
 
 
-def make_lines(points: List[Tuple[float, float]], point_distance_threshold: float = 2) -> List[Line]:
+def make_lines(points: List[Tuple[float, float]], point_distance_threshold: float = 2, min_line_length: float = 3) \
+        -> List[Line]:
     """
      * Creates multiple line segments from the points of the contour.
+    :param min_line_length: Lines with a length below this value will be ignored
     :param points:
     :param point_distance_threshold: Points up to a distance of this value will be considered neighbours
     :return:
     """
 
+    points = points.copy()
     lines: List[Line] = []
     while points:
         seg: List[Tuple[float, float]] = []
@@ -114,8 +117,9 @@ def make_lines(points: List[Tuple[float, float]], point_distance_threshold: floa
             x2 = float(x + dist/2 * vx)
             y1 = float(y - dist/2 * vy)
             y2 = float(y + dist/2 * vy)
-            # lines.append([(x1, y1), (x2, y2)])
-            lines.append(Line(nr=len(lines), p1=(x1, y1), p2=(x2, y2)))
+            line = Line(nr=len(lines), p1=(x1, y1), p2=(x2, y2))
+            if line.length >= min_line_length:
+                lines.append(line)
     return lines
 
 
@@ -225,7 +229,7 @@ def get_angle(first_line: Tuple[Tuple[float, float], Tuple[float, float]],
     :return:
     """
     if not second_line:
-        second_line = ((0, 0), (1, 0)) # horizontal vector (numpy indexing)
+        second_line = ((0, 0), (1, 0))  # horizontal vector (numpy indexing)
     v1 = np.array(second_line[0][::-1]) - np.array(second_line[1][::-1])
     v0 = np.array(first_line[0][::-1]) - np.array(first_line[1][::-1])
     angle = np.math.atan2(np.linalg.det([v0, v1]), np.dot(v0, v1))
@@ -248,8 +252,75 @@ def root_mean_square_error(p1, p2) -> float:
     return math.sqrt(1 / 2 * (mean_x + mean_y))
 
 
-def rectangularize(contour: List[Tuple[float, float]]) -> List[Tuple[float, float]]:
-    raise RuntimeError("NotImplemented")
+def get_reoriented_lines(lines: List[Line]) -> List[Line]:
+    outline = []
+    grouped_orientation = groupby(lines, key=lambda l: l.orientation)
+    for k, g in grouped_orientation:
+        subgroup = list(g)
+        for seg in subgroup:
+            nr = seg.nr
+
+            current_angle = get_angle((seg.p1, seg.p2))
+            if not seg.orthogonal:
+                target_angle = k
+            else:
+                target_angle = (k + 90)
+
+            angle_delta = target_angle - current_angle
+            angle_delta = min(angle_delta, angle_delta % 180)
+            ls = rotate(geometry.LineString(seg.coords), angle_delta)
+
+            xs = ls.coords.xy[0]
+            ys = ls.coords.xy[1]
+
+            fitted_line_new = [(xs[0], ys[0]), (xs[1], ys[1])]
+
+            new_line = Line(nr, *fitted_line_new)
+            new_line.set_orientation(target_angle)
+            outline.append(new_line)
+
+            # This step is important: The points are arranged in CCW order
+    lines_reordered = [outline.pop()]
+    while outline:
+        line = outline.pop()
+        p_1 = min(line.coords, key=lambda p: geometry.Point(p).distance(geometry.Point(lines_reordered[-1].p2)))
+        p_2 = line.coords[1] if p_1 == line.coords[0] else line.coords[0]
+        next_line = Line(line.nr, p_1, p_2)
+        next_line.set_orientation(line.orientation)
+        lines_reordered.append(next_line)
+    return lines_reordered
+
+
+def get_corner_points(outline: List[Line]) -> List[Tuple[float, float]]:
+    corner_points = []
+    for i, line in enumerate(outline):
+        next_line = outline[(i + 1) % len(outline)]
+        parallel = line.orientation == next_line.orientation
+        ls_1 = LineString(line.coords)
+        ls_2 = LineString(next_line.coords)
+        if ls_1.intersects(ls_2):
+            p = ls_1.intersection(ls_2)
+            corner_points.append((p.x, p.y))
+        elif parallel:
+            ls_middle = LineString([line.coords[-1], next_line.coords[0]])
+            center = ls_middle.centroid
+            center_line = scale(rotate(geom=LineString([center, (center.x + 10, center.y)]),
+                                       angle=line.orientation + 90,
+                                       origin=center), 10, 10)
+
+            p_1 = scale(ls_1, 1000, 1000).intersection(center_line)
+            p_2 = scale(ls_2, 1000, 1000).intersection(center_line)
+            corner_points.append((p_1.x, p_1.y))
+            corner_points.append((p_2.x, p_2.y))
+        else:
+            p = scale(ls_1, 1000, 1000).intersection(scale(ls_2, 1000, 1000))
+            corner_points.append((p.x, p.y))
+
+    corner_points.append(corner_points[0])
+    return corner_points
+
+
+def rectangularize(contour_points: List[Tuple[int, int]]) -> List[Tuple[float, float]]:
     # Tolerance for initial douglas-peucker run
     approximization_tolerance = 0.01
 
@@ -257,7 +328,7 @@ def rectangularize(contour: List[Tuple[float, float]]) -> List[Tuple[float, floa
     point_distance_threshold = 2
 
     # Lines below this length will be discarded
-    line_length_threshold = 3
+    min_line_length = 3
 
     # Angles with a difference up to this value will be considered parallel
     angle_parallelity_threshold = 20
@@ -271,8 +342,15 @@ def rectangularize(contour: List[Tuple[float, float]]) -> List[Tuple[float, floa
     # If the probability of a segment to its class is below this threshold, it will be reassigned to the most probable class
     reassignment_threshold = 0.25
 
-    lines = make_lines(contour.copy(), 2)
-    assign_orientation(lines.copy(), angle_parallelity_threshold=angle_parallelity_threshold)
+    contour_approximate = approximate_polygon(np.array(contour_points), tolerance=approximization_tolerance)
+    points = list(map(lambda t: (t[0], t[1]), contour_approximate.tolist()))
+    lines = make_lines(points, point_distance_threshold=point_distance_threshold, min_line_length=min_line_length)
+    assign_orientation(lines, angle_parallelity_threshold=angle_parallelity_threshold)
+    assign_neighbourhood(lines, neighbour_distance_threshold=neighbour_distance_threshold)
+    update_neighbourhoods(lines, window_size=window_size, reassignment_threshold=reassignment_threshold)
+    lines = get_reoriented_lines(lines)
+    corner_points = get_corner_points(lines)
+    return corner_points
 
 
 class MarchingSquares:
@@ -302,14 +380,11 @@ class MarchingSquares:
     def from_array(cls, data: np.ndarray):
         return cls(data)
 
-    def find_contour(self, approximization_tolerance: float = 0.01) -> Iterable[Tuple[int, int]]:
+    def find_contour(self) -> Iterable[Tuple[int, int]]:
         """
          * Returns the first contour found.
-        :param approximization_tolerance: tolerance for the douglas-peucker approximization run on the resulting points
         :return:
         """
-        if approximization_tolerance is None:
-            approximization_tolerance = 0.01
 
         self._calc_cell_states()
         points = []
@@ -331,13 +406,6 @@ class MarchingSquares:
                     raise RuntimeError("Invalid contour")
                 self._contour[self._sum_tuple(current_pos, (1, 1))] = 255
                 points.append(flipped)
-        if approximization_tolerance:
-            c = approximate_polygon(np.array(points), tolerance=approximization_tolerance)
-            points = list(map(lambda t: (t[0], t[1]), c.tolist()))
-
-        # ls = LineString()
-        # p = geometry.Point()
-        # p.di
 
         self._contour = np.zeros(self.img.shape, dtype=np.uint8)
         for x, y in points:
@@ -354,42 +422,8 @@ class MarchingSquares:
     def exact_contour(self):
         return self._contour
 
-    @property
-    def starting_point(self):
-        pass
-
-    def _get_lines(self, lineimg, main_orientation=None) -> Iterable:
-        # angles = [main_orientation-np.radians(90), main_orientation+np.radians(90), main_orientation]
-        # angles = [main_orientation]
-
-        lines = []
-        rows, cols = self.exact_contour.shape
-        # hspace, angles, dists = hough_line(img=self.exact_contour, theta=np.asarray(angles))
-        for _, theta, rho in zip(*hough_line(img=self.exact_contour)):
-            # y0 = (rho - 0 * np.cos(theta)) / np.sin(theta)
-            # y1 = (rho - cols * np.cos(theta)) / np.sin(theta)
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            x1 = int(x0 + 1000 * -b)
-            y1 = int(y0 + 1000 * a)
-            x2 = int(x0 - 1000 * -b)
-            y2 = int(y0 - 1000 * a)
-            lines.append((x1, y1, x2, y2))
-            print(x1, y1, x2, y2)
-            cv2.line(lineimg, (x1, y1), (x2, y2), 255, 1)
-
-        im = Image.fromarray(lineimg, mode="L")
-        im.save("hough.bmp")
-        return lines
-        # for h in hspace:
-        #     print("h: ", h)
-        #
-        # for a in angles:
-        #     print("a: ", np.degrees(a))
-
-    def _get_main_orientation(self, lineimg, angle: float = None, angle_in_degrees: bool = False, max_lines:int = None) -> int:
+    def _get_main_orientation(self, angle: float = None, angle_in_degrees: bool = False, max_lines: int = None)\
+            -> Tuple[float, Tuple[float, float]]:
         max_threshold = 5
         lines = None
         # ang = np.pi / 180 if angle is None else angle
@@ -407,11 +441,8 @@ class MarchingSquares:
                 break
             max_threshold += 1
 
-        print("max threshold: ", max_threshold)
-        # lines = cv2.HoughLines(image=self.exact_contour, rho=1, theta=np.pi / 180, threshold=20)
         angles = {}
         maxcount = 0
-        # lineimg = np.zeros(self.img.shape, dtype=np.uint8)
         nearest_point = None
         weighted_avg = None
         if lines is not None:
@@ -436,7 +467,6 @@ class MarchingSquares:
                     y1 = int(y0 + 1000 * a)
                     x2 = int(x0 - 1000 * -b)
                     y2 = int(y0 - 1000 * a)
-                    cv2.line(lineimg, (x1, y1), (x2, y2), 255, 1)
                     # if not nearest_point:
                     #     dist = None
                     #     ls = LineString([(x1, y1), (x2, y2)])
@@ -454,83 +484,14 @@ class MarchingSquares:
                 counts += angles[a]
             weighted_avg = int(round(angle_sum / counts))
             weighted_avg %= 180 if angle_in_degrees else np.pi
-        return weighted_avg
+        return weighted_avg, nearest_point
 
     def main_orientation(self, angle_in_degrees: bool = False) -> Tuple[int, geometry.Point]:
         if not self._marched:
             raise RuntimeError("To get the main orientation, run 'find_contour' first.")
 
-        # all_lines = []
-        #
-        # max_threshold = 5
-        # lines = None
-        # while True:
-        #     new_lines = cv2.HoughLines(image=self.exact_contour, rho=1, theta=np.pi / 180, threshold=max_threshold)
-        #     if new_lines is not None:
-        #         lines = new_lines
-        #     else:
-        #         max_threshold -= 1
-        #         break
-        #     # break
-        #     max_threshold += 1
-        # all_lines.extend(lines)
-        #
-        # print("max threshold: ", max_threshold)
-        # # lines = cv2.HoughLines(image=self.exact_contour, rho=1, theta=np.pi / 180, threshold=20)
-        # angles = {}
-        # maxcount = 0
-        # lineimg = np.zeros(self.img.shape, dtype=np.uint8)
-        # nearest_point = None
-        # if lines is not None:
-        #     for l in lines:  # rho = distance, theta = angle
-        #         for rho, theta in l:
-        #             if angle_in_degrees:
-        #                 angle = int(math.degrees(theta))
-        #             else:
-        #                 angle = theta
-        #             if angle not in angles:
-        #                 angles[angle] = 0
-        #             newcount = angles[angle] + 1
-        #             angles[angle] = newcount
-        #             if newcount > maxcount:
-        #                 maxcount = newcount
-        #
-        #             a = np.cos(theta)
-        #             b = np.sin(theta)
-        #             x0 = a * rho
-        #             y0 = b * rho
-        #             x1 = int(x0 + 1000 * -b)
-        #             y1 = int(y0 + 1000 * a)
-        #             x2 = int(x0 - 1000 * -b)
-        #             y2 = int(y0 - 1000 * a)
-        #             if not nearest_point:
-        #                 dist = None
-        #                 ls = LineString([(x1, y1), (x2, y2)])
-        #                 for px, py in self._points:
-        #                     p = geometry.Point(px, py)
-        #                     new_dist = p.distance(ls)
-        #                     if not nearest_point or new_dist < dist:
-        #                         nearest_point = p
-        #                         dist = new_dist
-        #             # cv2.line(lineimg, (x1, y1), (x2, y2), 255, 1)
-        #
-        #     angle_sum = 0
-        #     counts = 0
-        #     for a in angles:
-        #         angle_sum += a*angles[a]
-        #         counts += angles[a]
-        #     weighted_avg = int(round(angle_sum / counts))
-        #
-        #     self._get_lines(lineimg=lineimg, main_orientation=np.radians(weighted_avg))
-
-        lineimg = np.zeros(self.img.shape, dtype=np.uint8)
         nearest_point: geometry.Point = None
-        weighted_avg: int = self._get_main_orientation(lineimg, angle_in_degrees=angle_in_degrees)
-        # weighted_avg2: int = self._get_main_orientation(lineimg, max_lines=20)
-
-        # cv2.polylines(lineimg, np.asarray([self._points], dtype=np.int32), color=255, isClosed=True)
-        # im = Image.fromarray(lineimg, mode="L")
-        # im.save("hough.bmp")
+        weighted_avg: int = self._get_main_orientation(angle_in_degrees=angle_in_degrees)
         return weighted_avg, nearest_point
 
     @staticmethod
