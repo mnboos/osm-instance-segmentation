5a94828d94626d8af1229521bc19a4f1675c5c1e
samwisee <mboos@outlook.com>
2018-03-02 13:09:49 +0100

    airtiler config updated

--- core/generate_training_data.py
+++ core/generate_training_data.py
@@ -2,9 +2,11 @@ import json
 import os
 import secrets
 from airtiler import Airtiler
+from core.settings import IMAGE_OUTPUT_FOLDER
 
 if __name__ == "__main__":
     with open(os.path.join(os.getcwd(), "core", "airtiler_config.json"), 'r') as f:
         config = json.load(f)
+    config["options"]["target_dir"] = IMAGE_OUTPUT_FOLDER
     airtiler = Airtiler(secrets.BING_KEY)
     airtiler.process(config)
