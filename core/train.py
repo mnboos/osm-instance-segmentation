406bf50cf5e9e2bafaa04f61aec693d557831c43
samwisee <mboos@outlook.com>
2018-03-02 11:03:44 +0100

    use coco model

--- core/train.py
+++ core/train.py
@@ -60,7 +60,7 @@ def train():
     model = modellib.MaskRCNN(mode="training", config=config, model_dir=MODEL_DIR)
 
     # Which weights to start with?
-    init_with = "last"  # imagenet, coco, or last
+    init_with = "coco"  # imagenet, coco, or last
 
     if init_with == "imagenet":
         model.load_weights(model.get_imagenet_weights(), by_name=True)
